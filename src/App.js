e64df8cce2b7943dea085a1ed04f19f3536f9b6c
Miqueas <miqueaslazarte03@gmail.com>
2023-03-02 13:49:04 -0300

    fixs

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import ContactRenderComp from './components/container/contact_list';
+/* import Greeting from './components/pure/greeting'; */
+/* import GreetingF from './components/pure/greetingF'; */
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <Greeting name="Moic"></Greeting> */}
+        {/* Componente funcional */}
+        {/* <GreetingF name="mIKIKI"></GreetingF> */}
+        {/* {Componente de listado de tareas} */}
+        <ContactRenderComp></ContactRenderComp>
       </header>
     </div>
   );
